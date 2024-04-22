a6d380413a3fbafce916b68062e15795826f33f6
Mathieu Agostini <maagosti@c2r1p6.42nice.fr>
2024-04-22 16:30:44 +0200

    fix tests

--- tests/ft_memmove/test01_int.c
+++ tests/ft_memmove/test01_int.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 int main(void)
 {
 	int	bytes[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0};
 	int	src[10] = {42, 43, 44};
 
-	ft_memcpy(bytes, src, 3 * sizeof(int));
+	ft_memmove(bytes, src, 3 * sizeof(int));
 	for (int i = 0; i < 10; i++)
 		printf("%d ", bytes[i]);
 }
--- tests/ft_memmove/test03_size_ten.c
+++ tests/ft_memmove/test03_size_ten.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 int main(void)
 {
 	char	bytes[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0};
 	char	src[10] = {42, 43, 44};
 
-	ft_memcpy(bytes, src, 10);
+	ft_memmove(bytes, src, 10);
 	for (int i = 0; i < 10; i++)
 		printf("%d ", bytes[i]);
 }
