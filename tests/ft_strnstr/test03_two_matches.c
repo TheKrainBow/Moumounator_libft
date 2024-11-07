b4e993ce643946187ebf74cc97c6cc4e468bd1d0
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2024-11-07 18:36:08 +0100

    Added definition for strnstr to avoid crashes

--- tests/ft_strnstr/test03_two_matches.c
+++ tests/ft_strnstr/test03_two_matches.c
@@ -1,13 +1,37 @@
-#include <stdio.h>
 #include <limits.h>
+#include <stdio.h>
+#include <string.h>
 
-#define TESTED_STRING1 "Trouverez vous Charly dans cette phrase? Attention, le second Charly est un piège!"
+char	*strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	needle_len;
+
+	needle_len = strlen(needle);
+	if (needle_len > len)
+	{
+		return (NULL);
+	}
+	for (size_t i = 0; i <= len - needle_len; i++)
+	{
+		if (strncmp(haystack + i, needle, needle_len) == 0)
+		{
+			return (char *)(haystack + i);
+		}
+	}
+	return (NULL);
+}
+
+#define TESTED_STRING1 "Trouverez vous Charly dans cette phrase? Attention,
+	le second Charly est un piège!"
 #define TESTED_STRING2 "Charly"
 #define TESTED_SIZE 83
 
 char	*ft_strnstr(const char *big, const char *little, size_t n);
 
-int main(void) {
-	printf("strnstr(%s, %s, %d) returned \"%s\"\n", TESTED_STRING1, TESTED_STRING2, TESTED_SIZE, ft_strnstr(TESTED_STRING1, TESTED_STRING2, TESTED_SIZE));
+int	main(void)
+{
+	printf("strnstr(%s, %s, %d) returned \"%s\"\n", TESTED_STRING1,
+		TESTED_STRING2, TESTED_SIZE, ft_strnstr(TESTED_STRING1, TESTED_STRING2,
+			TESTED_SIZE));
 	return (0);
 }
--- tests/ft_strnstr/test06_negative.c
+++ tests/ft_strnstr/test06_negative.c
@@ -1,5 +1,25 @@
-#include <stdio.h>
 #include <limits.h>
+#include <stdio.h>
+#include <string.h>
+
+char	*strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	needle_len;
+
+	needle_len = strlen(needle);
+	if (needle_len > len)
+	{
+		return (NULL);
+	}
+	for (size_t i = 0; i <= len - needle_len; i++)
+	{
+		if (strncmp(haystack + i, needle, needle_len) == 0)
+		{
+			return (char *)(haystack + i);
+		}
+	}
+	return (NULL);
+}
 
 #define TESTED_STRING1 "Trouverez vous Charly dans cette phrase?"
 #define TESTED_STRING2 "Charly"
@@ -7,7 +27,10 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t n);
 
-int main(void) {
-	printf("strnstr(%s, %s, %d) returned \"%s\"\n", TESTED_STRING1, TESTED_STRING2, TESTED_SIZE, ft_strnstr(TESTED_STRING1, TESTED_STRING2, TESTED_SIZE));
+int	main(void)
+{
+	printf("strnstr(%s, %s, %d) returned \"%s\"\n", TESTED_STRING1,
+		TESTED_STRING2, TESTED_SIZE, ft_strnstr(TESTED_STRING1, TESTED_STRING2,
+			TESTED_SIZE));
 	return (0);
 }
